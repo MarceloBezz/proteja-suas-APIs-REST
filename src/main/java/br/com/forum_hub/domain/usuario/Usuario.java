d1b9414af49c5fc0da31b190d934e28cfac80f0b
Marcelo <mdbezerra16@gmail.com>
2025-05-17 09:28:30 -0300

    feat: :sparkles: Configuração da classe de Usuário

---
+++ src/main/java/br/com/forum_hub/domain/usuario/Usuario.java
@@ -0,0 +1,71 @@
+package br.com.forum_hub.domain.usuario;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nomeCompleto;
+    private String nomeUsuario;
+    private String email;
+    private String senha;
+    private String biografia;
+    private String miniBiografia;
+
+    public Usuario(String nomeCompleto, String nomeUsuario, String email, String senha, String biografia,
+            String miniBiografia) {
+        this.nomeCompleto = nomeCompleto;
+        this.nomeUsuario = nomeUsuario;
+        this.email = email;
+        this.senha = senha;
+        this.biografia = biografia;
+        this.miniBiografia = miniBiografia;
+    }
+
+    @SuppressWarnings("unused")
+    private Usuario() {
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public String getBiografia() {
+        return biografia;
+    }
+
+    public String getMiniBiografia() {
+        return miniBiografia;
+    }
+}
