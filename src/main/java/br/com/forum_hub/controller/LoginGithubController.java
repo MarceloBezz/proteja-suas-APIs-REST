e1c8024d92b17c7e928bdcf6016075fece5ed078
Marcelo <mdbezerra16@gmail.com>
2025-05-27 22:03:26 -0300

    feat: :sparkles: Implementação do fluxo de autenticação com GitHub, incluindo obtenção de email e tokens de acesso

--- src/main/java/br/com/forum_hub/controller/LoginGithubController.java
+++ src/main/java/br/com/forum_hub/controller/LoginGithubController.java
@@ -3,40 +3,57 @@ package br.com.forum_hub.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.forum_hub.domain.autenticacao.DadosToken;
+import br.com.forum_hub.domain.autenticacao.TokenService;
 import br.com.forum_hub.domain.autenticacao.github.LoginGithubService;
+import br.com.forum_hub.domain.usuario.UsuarioRepository;
 
 import java.net.URI;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @RestController
-@RequestMapping("/login/github")
 public class LoginGithubController {
-    
-    private final LoginGithubService loginGithubService;
 
-    public LoginGithubController(LoginGithubService loginGithubService) {
+    private final LoginGithubService loginGithubService;
+    private final UsuarioRepository usuarioRepository;
+    private final TokenService tokenService;
+
+    public LoginGithubController(LoginGithubService loginGithubService, UsuarioRepository usuarioRepository, TokenService tokenService) {
         this.loginGithubService = loginGithubService;
+        this.usuarioRepository = usuarioRepository;
+        this.tokenService = tokenService;
     }
 
-    @GetMapping
+    @GetMapping("/login/github")
     public ResponseEntity<Void> redirecionarGithub() {
-        //url
+        // url
         var headers = new HttpHeaders();
         headers.setLocation(URI.create(loginGithubService.gerarUrl()));
 
         return new ResponseEntity<>(headers, HttpStatus.FOUND);
     }
 
-    @GetMapping("autorizado")
-    public ResponseEntity<String> getMethodName(@RequestParam String code) {
-        var token = loginGithubService.obterToken(code);
-        return ResponseEntity.ok(token);
+    @GetMapping("/login/github/autorizado")
+    public ResponseEntity<DadosToken> autenticarUsuarioOAuth(@RequestParam String code) {
+        var email = loginGithubService.obterEmail(code);
+
+        var usuario = usuarioRepository.findByEmailIgnoreCase(email)
+                .orElseThrow();
+        var authentication = new UsernamePasswordAuthenticationToken(usuario, null,
+                usuario.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String tokenAcesso = tokenService.gerarToken(usuario);
+        String refreshToken = usuario.novoRefreshToken();
+
+        return ResponseEntity.ok(new DadosToken(tokenAcesso, refreshToken));
     }
-    
+
 }
